6df002f9e56e3ec9d924b0b8f1b483d6a84f211a
sdhope <dhopesrushti@gmail.com>
2014-05-03 22:44:11 -0400

    Update Dirac.m

--- Dirac.m
+++ Dirac.m
@@ -1,11 +1,5 @@
-% This MATLAB code is made as part of the course project for 
-% COMP 765 - Advanced Topics (Mobile Robotics) at McGill University, Canada
-% It demonstrates the calculation of the Dirac delta function term for the
+% This MATLAB code demonstrates the calculation of the Dirac delta function term for the
 % level set evolution
-%
-% Author: Srushti Dhope (srushti.dhope@mail.mcgill.ca)
-%
-% Date: April 29th, 2013
 
 function [ deltaPhi ] = Dirac( phi, epsilon )
 
