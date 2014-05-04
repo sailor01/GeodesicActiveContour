f9a364ccb6e43c54abd537aac9372b9354d62e4f
sdhope <dhopesrushti@gmail.com>
2014-05-03 22:46:16 -0400

    Update distReg.m

--- distReg.m
+++ distReg.m
@@ -1,11 +1,5 @@
-% This MATLAB code is made as part of the course project for 
-% COMP 765 - Advanced Topics (Mobile Robotics) at McGill University, Canada
-% It demonstrates the calculation of the distance regularization term for
+% This MATLAB code demonstrates the calculation of the distance regularization term for
 % the level set evolution
-%
-% Author: Srushti Dhope (srushti.dhope@mail.mcgill.ca)
-%
-% Date: April 29th, 2013
 
 function [ dReg ] = distReg( phi )
 
